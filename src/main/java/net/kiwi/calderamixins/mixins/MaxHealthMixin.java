178310a89648c841a8e28688adff0d3401a05ba2
kiwiphoenix <kiwiphoenix364@gmail.com>
2024-08-22 01:00:51 -0400

    clean up

--- src/main/java/net/kiwi/calderamixins/mixins/MaxHealthMixin.java
+++ src/main/java/net/kiwi/calderamixins/mixins/MaxHealthMixin.java
@@ -1,18 +1,11 @@
 package net.kiwi.calderamixins.mixins;
 
-import net.minecraft.world.entity.player.Player;
 import net.silentchaos512.scalinghealth.utils.config.SHItems;
 import net.silentchaos512.scalinghealth.utils.config.SHPlayers;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import net.minecraft.world.entity.player.Player;
-import net.silentchaos512.scalinghealth.capability.IPlayerData;
-import net.silentchaos512.scalinghealth.capability.PlayerDataCapability;
-import net.silentchaos512.scalinghealth.config.EvalVars;
-import net.silentchaos512.scalinghealth.resources.mechanics.PlayerMechanics;
-import net.silentchaos512.scalinghealth.resources.mechanics.SHMechanics;
 import static net.silentchaos512.scalinghealth.utils.config.SHPlayers.maxHealth;
 import static net.silentchaos512.scalinghealth.utils.config.SHPlayers.startingHealth;
 
